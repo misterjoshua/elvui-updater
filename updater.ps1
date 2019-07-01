a62a775b86183b75ac1326d1b256f25f31e4c50c
Josh Kellendonk <josh@colloco.com>
2019-07-01 15:43:46 -0600

    Add support for the client api's url and make it the default

--- updater.ps1
+++ updater.ps1
@@ -3,13 +3,32 @@ param (
     [string] $InstallPath
 )
 
-function Get-LatestDownloadHref {
-    $request = Invoke-WebRequest $updatePage
-    $downloadLink = $request.Links | Where-Object { $_.href -like "*elvui*.zip" } | Select -First 1
-    $href = $downloadBaseUrl + $downloadLink.href
+enum DownloadHrefMethod {
+    ClientApi = "ClientApi"
+    PageScrape = "PageScrape"
+}
+
+function Get-LatestDownloadHref([DownloadHrefMethod] $method = [DownloadHrefMethod]::ClientApi) {
+    switch ($method) {
+        "ClientApi" { $href = Get-LatestDownloadHrefFromApi; continue }
+        "PageScrape" { $href = Get-LatestDownloadHrefFromPage; continue }
+        Default { throw "Unsupported method: $method" }
+    }
 
     Write-Host @infoStyle "The download href is $href"
     $href
+}
+
+# Get the latest download url from the tukui client api
+function Get-LatestDownloadHrefFromApi {
+    ((Invoke-WebRequest $clientApiUrl).Content | ConvertFrom-Json).url
+}
+
+# Get the latest download url by scraping the download page
+function Get-LatestDownloadHrefFromPage {
+    $request = Invoke-WebRequest $updatePage
+    $downloadLink = $request.Links | Where-Object { $_.href -like "*elvui*.zip" } | Select -First 1
+    $downloadBaseUrl + $downloadLink.href
 }
 
 function Get-ZipLocalPath($downloadHref) {
@@ -26,6 +45,7 @@ $ErrorActionPreference = "Stop"
 
 $downloadBaseUrl = "https://www.tukui.org"
 $updatePage = "https://www.tukui.org/download.php?ui=elvui"
+$clientApiUrl = "https://www.tukui.org/client-api.php?ui=elvui"
 $infoStyle = @{Foreground = "Cyan"}
 
 try {
